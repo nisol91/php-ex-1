dc5b5c8966a9d17c0ff3c682bd7d2c1106dff85f
nisol91 <nicolasolzi91@gmail.com>
2019-01-21 14:16:13 +0100

    fine

--- php-ex-1.php
+++ php-ex-1.php
@@ -1,5 +1,28 @@
-<?php
- 
-
-
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $testo = 'Ciao, io mi chiamo Nicola';
+      echo $testo;
+      ?>
+    </p>
+    <p>
+      <?php
+        $lungStr = strlen($testo);
+        echo $lungStr;
+      ?>
+    </p>
+    <p>
+      <?php
+        $badWord = $_GET['name'];
+        $strSubst = str_replace($badWord, "***", $testo);
+        echo $strSubst;
+      ?>
+    </p>
+  </body>
+</html>
